18388ccc3b30c6bf53fb4feeda4b1ef554a663e8
Rubens Santo <r4454406@gmail.com>
2023-08-21 15:06:56 -0300

    Ajuste na mensagem

--- Tabuada/Tabuada/Program.cs
+++ Tabuada/Tabuada/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TabuadaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número para a tabuada (0 a 10): ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A tabuada solicitada foi: {numero}\n");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+        }
+    }
+}
+
